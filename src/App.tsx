a948a89a5cec2b472fbfc0a819878f2569881976
matos795 <matosalexandre795@gmail.com>
2025-12-05 13:13:17 -0300

    New Route Home, Routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './routes/Home'
+
 
 export default function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}>
+          
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
